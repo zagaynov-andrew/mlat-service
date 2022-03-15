5f44943bd2b194fc2f31f416f9b0228b4899649e
zagaynov-andrew <zagaynov.drew@gmail.com>
2022-03-15 12:46:43 +0300

    Add Location2D, Station

---
+++ src/main/java/datacenter/Station.java
@@ -0,0 +1,17 @@
+package datacenter;
+
+import shared.Location;
+
+public class Station {
+
+    protected int id;
+    protected Location location;
+
+    public int getId() {
+        return id;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+}
---
+++ src/main/java/shared/Location2D.java
@@ -0,0 +1,11 @@
+package shared;
+
+public class Location2D extends Location{
+
+    public float y;
+
+    Location2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
